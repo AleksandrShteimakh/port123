3e6e15155184b93dea479ade013de3fb67b4f70e
AleksandrShteimakh <Ekklesiast8100@gmail.com>
2023-07-08 00:39:23 +0200

    add

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ScrollToTop from './utils/ScrollToTop';
 function App() {
   return (
     <div className="App">
-      <Router basename="/fine">
+      <Router basename="/port123">
         <ScrollToTop />
         <Navbar />
 
